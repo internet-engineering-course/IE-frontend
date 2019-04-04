1124d87e63f4a787ebdb7d67ed13741cb84d0818
Ahmadreza-SY <ahmadreza.saboor2012@gmail.com>
2019-04-04 20:49:16 +0430

    remove unecessary components

--- src/views/profile/Profile.tsx
+++ src/views/profile/Profile.tsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import Header from "src/views/common/Header.tsx";
-import Footer from "src/views/common/Footer.tsx";
-import Bar from "src/views/common/bar/Bar.tsx";
-import ProfileBar from "src/views/common/bar/ProfileBar.tsx";
+import Header from "src/views/common/Header";
+import Footer from "src/views/common/Footer";
+import Bar from "src/views/common/bar/Bar";
+import ProfilePhoto from "src/resources/img/profile.jpg";
+import "./Profile.css";
 
 export default class Profile extends Component<Props, State> {
 	constructor(props: Props) {
@@ -14,8 +15,31 @@ export default class Profile extends Component<Props, State> {
 				<Header isUserLoggedIn={true} />
 				<main>
 					<Bar height={"150px"}>
-						<ProfileBar />
+						<div className="container h-100">
+							<div className="row h-100 align-items-end no-gutters">
+								<div className="col-auto">
+									<span className="profile-bar dark-bar" />
+								</div>
+							</div>
+						</div>
 					</Bar>
+					<section className="container">
+						<div className="row align-items-center">
+							<div className="col-auto">
+								<div className="profile-border">
+									<img
+										src={ProfilePhoto}
+										alt="Profile Photo"
+										className="profile-image"
+									/>
+								</div>
+							</div>
+							<div className="col-auto">
+								<h2>نوید اکبری</h2>
+								<p style={{ color: "#787878" }}>دانشجو</p>
+							</div>
+						</div>
+					</section>
 				</main>
 				<Footer />
 			</div>
