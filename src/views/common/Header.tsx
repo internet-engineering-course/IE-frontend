e90b0da2eb52e7b36a4e772fea61ca7770b66e51
Ahmadreza-SY <ahmadreza.saboor2012@gmail.com>
2019-04-06 01:29:36 +0430

    fix head profile link

--- src/views/common/Header.tsx
+++ src/views/common/Header.tsx
@@ -1,66 +1,69 @@
-import React, { Component } from 'react'
-import 'src/views/common/Header.scss'
-import 'src/scss/style.scss'
-import logoUrl from 'src/resources/img/logo_v1.png'
+import React, { Component } from "react";
+import "src/views/common/Header.scss";
+import "src/scss/style.scss";
+import logoUrl from "src/resources/img/logo_v1.png";
+import { DEFAULT_USER_ID } from "src/constants/constants";
 
 export default class Header extends Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+	}
 
-    constructor(props: Props) {
-        super(props);
-    }
+	render() {
+		const isUserLoggedIn: boolean = this.props.isUserLoggedIn;
+		if (isUserLoggedIn) {
+			const result = this.userHeader();
+			return result;
+		} else {
+			const result = this.guestHeader();
+			return result;
+		}
+	}
 
-    render() {
-        const isUserLoggedIn: boolean = this.props.isUserLoggedIn;
-        if (isUserLoggedIn) {
-            const result = this.userHeader();
-            return result;
-        }
-        else {
-            const result = this.guestHeader();
-            return result;
-        }
-    }
+	guestHeader = () => {
+		return (
+			<header>
+				<div className="header">
+					<div className="container header-hover">
+						<div className="row justify-content-between align-items-center">
+							<div id="logo" className="col-auto">
+								<img src={logoUrl} alt="jobonja-logo" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+		);
+	};
 
-    guestHeader = () => {
-        return (
-            <header>
-                <div className="header">
-                    <div className="container header-hover">
-                        <div className="row justify-content-between align-items-center">
-                            <div id="logo" className="col-auto">
-                                <img src={logoUrl} alt="jobonja-logo" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </header>
-        );
-    }
-
-    userHeader = () => {
-        return (
-            <header>
-                <div className="header">
-                    <div className="container header-hover">
-                        <div className="row justify-content-between align-items-center">
-                            <div id="logo" className="col-auto">
-                                <img src={logoUrl} alt="jobonja-logo" />
-                            </div>
-                            <nav className="col-auto">
-                                <div className="row align-items-center">
-                                    <a href="../views/profile.html" className="col-auto profile-link">حساب کاربری</a>
-                                    <div className="col-auto">خروج</div>
-                                </div>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </header>
-        );
-    }
+	userHeader = () => {
+		return (
+			<header>
+				<div className="header">
+					<div className="container header-hover">
+						<div className="row justify-content-between align-items-center">
+							<div id="logo" className="col-auto">
+								<img src={logoUrl} alt="jobonja-logo" />
+							</div>
+							<nav className="col-auto">
+								<div className="row align-items-center">
+									<a
+										href={"/profile/" + DEFAULT_USER_ID}
+										className="col-auto profile-link">
+										حساب کاربری
+									</a>
+									<div className="col-auto">خروج</div>
+								</div>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</header>
+		);
+	};
 }
 
 interface Props {
-    isUserLoggedIn: boolean
+	isUserLoggedIn: boolean;
 }
-interface State { }
+interface State {}
